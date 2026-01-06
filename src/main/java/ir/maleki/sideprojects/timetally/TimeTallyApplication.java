8fe54d39fd3b1278f20e8544304177374b27b296
Rasool Malekinejad <rasool.malekinejad@gmail.com>
2026-01-06 18:50:30 +0330

    Bootstrap TimeTally Spring Boot application

---
+++ src/main/java/ir/maleki/sideprojects/timetally/TimeTallyApplication.java
@@ -0,0 +1,13 @@
+package ir.maleki.sideprojects.timetally;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeTallyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeTallyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ir/maleki/sideprojects/timetally/TimeTallyApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.maleki.sideprojects.timetally;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeTallyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

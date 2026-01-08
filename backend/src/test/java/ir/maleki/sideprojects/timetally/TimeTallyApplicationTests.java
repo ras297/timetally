38185b0214d367d7737c9e0910ef47986f808940
Rasool Malekinejad <rasool.malekinejad@gmail.com>
2026-01-08 13:14:46 +0330

    Convert tabs to 4 spaces and add .editorconfig to standardize code style

--- backend/src/test/java/ir/maleki/sideprojects/timetally/TimeTallyApplicationTests.java
+++ backend/src/test/java/ir/maleki/sideprojects/timetally/TimeTallyApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TimeTallyApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }

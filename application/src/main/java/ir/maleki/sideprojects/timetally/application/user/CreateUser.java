7ac1d54bec9ed842744bf8ab987d3eff80a42450
Rasool Malekinejad <rasool.malekinejad@gmail.com>
2026-02-05 02:06:39 +0330

    Convert CreateUser DTO to a java record and improve its validation

--- application/src/main/java/ir/maleki/sideprojects/timetally/application/user/CreateUser.java
+++ application/src/main/java/ir/maleki/sideprojects/timetally/application/user/CreateUser.java
@@ -3,25 +3,12 @@ package ir.maleki.sideprojects.timetally.application.user;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public class CreateUser {
-    @NotBlank
-    @Size(min = 5, max = 10)
-    private final String username;
+import java.io.Serializable;
 
-    @NotBlank
-    @Size(min = 6)
-    private final String password;
-
+public record CreateUser(@NotBlank @Size(min = 5, max = 20) String username,
+                         @NotBlank @Size(min = 8, max = 20) String password) implements Serializable {
     public CreateUser(String username, String password) {
         this.username = username;
         this.password = password;
-    }
-
-    public String username() {
-        return username;
-    }
-
-    public String password() {
-        return password;
     }
 }
